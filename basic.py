bae949dac8d8ae0dc1df18b2bfb38274a15a2889
unknown <madhanreigns@gmail.com>
2023-11-23 23:32:48 +0530

    arithmetic

--- basic.py
+++ basic.py
@@ -1,6 +1,15 @@
 def add(a,b):
     return a+b
 
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
 a=int(input("Enter a Value:"))
 b=int(input("Enter b Value:"))
 print(f"The Addition Value Of a and b is : {add(a,b)}")
