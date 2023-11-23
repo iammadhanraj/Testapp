e3e537e5e2af5f2f9c021de77dc98a05a96c3d99
unknown <madhanreigns@gmail.com>
2023-11-24 00:08:23 +0530

    loop_creation

--- basic.py
+++ basic.py
@@ -10,7 +10,6 @@ def mul(a,b):
 def div(a,b):
     return a/b
 
-
 op='''
 1.add
 2.sub
@@ -18,10 +17,27 @@ op='''
 4.div
 5.exit
 '''
-
+  
 while True:
     print(op)
     ch=int(input("Enter Your Choice:"))
-    a=int(input("Enter a Value:"))
-    b=int(input("Enter b Value:"))
-    print(f"The Addition Value Of a and b is : {add(a,b)}")
+    if ch==1:
+        a=int(input("Enter a Value:"))
+        b=int(input("Enter b Value:"))
+        print(f"The Addition Value Of a and b is : {add(a,b)}") 
+    elif ch==2:
+        a=int(input("Enter a Value:"))
+        b=int(input("Enter b Value:"))
+        print(f"The Subtraction Value Of a and b is : {sub(a,b)}")
+    elif ch==3:
+        a=int(input("Enter a Value:"))
+        b=int(input("Enter b Value:"))
+        print(f"The Multiplication Value Of a and b is : {mul(a,b)}")
+    elif ch==4:
+        a=int(input("Enter a Value:"))
+        b=int(input("Enter b Value:"))
+        print(f"The Division Value Of a and b is : {div(a,b)}")
+    elif ch==5:
+        exit()
+    else:
+        print("Something Went Wrong! Try Again")
