72c2641a686396d4be64002682a34ab7b363bdfd
unknown <madhanreigns@gmail.com>
2023-11-23 23:57:08 +0530

    loop_creation

--- basic.py
+++ basic.py
@@ -10,6 +10,7 @@ def mul(a,b):
 def div(a,b):
     return a/b
 
+
 op='''
 1.add
 2.sub
@@ -18,8 +19,9 @@ op='''
 5.exit
 '''
 
-ch=int(input("Enter Your Choice:"))
-
-a=int(input("Enter a Value:"))
-b=int(input("Enter b Value:"))
-print(f"The Addition Value Of a and b is : {add(a,b)}")
+while True:
+    print(op)
+    ch=int(input("Enter Your Choice:"))
+    a=int(input("Enter a Value:"))
+    b=int(input("Enter b Value:"))
+    print(f"The Addition Value Of a and b is : {add(a,b)}")
