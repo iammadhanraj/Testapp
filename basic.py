afb95652c180dc03f532511e60c8890241c5c715
unknown <madhanreigns@gmail.com>
2023-11-23 23:49:37 +0530

    choice creator

--- basic.py
+++ basic.py
@@ -10,6 +10,16 @@ def mul(a,b):
 def div(a,b):
     return a/b
 
+op='''
+1.add
+2.sub
+3.mul
+4.div
+5.exit
+'''
+
+ch=int(input("Enter Your Choice:"))
+
 a=int(input("Enter a Value:"))
 b=int(input("Enter b Value:"))
 print(f"The Addition Value Of a and b is : {add(a,b)}")
